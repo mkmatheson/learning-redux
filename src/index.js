a1001030f53f02bb4ce9e38f228f2015da120bf1
McKay Matheson <mckaymatheson@gmail.com>
2020-08-19 11:15:12 -0600

    unsubscribe from the store

--- src/index.js
+++ src/index.js
@@ -1,34 +1,20 @@
 import C from "./constants";
 import appReducer from "./store/reducers";
-// import initialState from "./initialState.json";
 import { createStore } from "redux";
 
-const initialState = localStorage["redux-store"]
-  ? JSON.parse(localStorage["redux-store"])
-  : {};
+const store = createStore(appReducer);
 
-const store = createStore(appReducer, initialState);
+const unsubscribeGoalLogger = store.subscribe(() =>
+  console.log(`   Goal: ${store.getState().goal}`)
+);
 
-window.store = store;
+setInterval(() => {
+  store.dispatch({
+    type: C.SET_GOAL,
+    payload: Math.floor(Math.random() * 100),
+  });
+}, 250);
 
-// store.subscribe(() => console.log(store.getState()));
-
-store.subscribe(() => {
-  const state = JSON.stringify(store.getState());
-  localStorage["redux-store"] = state;
-});
-
-// store.dispatch({
-//   type: C.ADD_DAY,
-//   payload: {
-//     resort: "Snowbird",
-//     date: "2020-09-09",
-//     powder: false,
-//     backcountry: true,
-//   },
-// });
-
-// store.dispatch({
-//   type: C.SET_GOAL,
-//   payload: 2,
-// });
+setTimeout(() => {
+  unsubscribeGoalLogger();
+}, 3000);

3475f6e71752eff06e9910e98286a55b485a3d49
McKay Matheson <mckaymatheson@gmail.com>
2020-08-19 11:06:46 -0600

    subscribe to the store and use localstorage

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import C from "./constants";
 import appReducer from "./store/reducers";
-import initialState from "./initialState.json";
+// import initialState from "./initialState.json";
 import { createStore } from "redux";
 
-const store = createStore(appReducer);
+const initialState = localStorage["redux-store"]
+  ? JSON.parse(localStorage["redux-store"])
+  : {};
 
-console.log("initial state", store.getState());
+const store = createStore(appReducer, initialState);
 
-store.dispatch({
-  type: C.ADD_DAY,
-  payload: {
-    resort: "Snowbird",
-    date: "2020-09-09",
-    powder: false,
-    backcountry: true,
-  },
+window.store = store;
+
+// store.subscribe(() => console.log(store.getState()));
+
+store.subscribe(() => {
+  const state = JSON.stringify(store.getState());
+  localStorage["redux-store"] = state;
 });
 
-console.log("next state", store.getState());
+// store.dispatch({
+//   type: C.ADD_DAY,
+//   payload: {
+//     resort: "Snowbird",
+//     date: "2020-09-09",
+//     powder: false,
+//     backcountry: true,
+//   },
+// });
+
+// store.dispatch({
+//   type: C.SET_GOAL,
+//   payload: 2,
+// });
